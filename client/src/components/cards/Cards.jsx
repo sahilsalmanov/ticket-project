3cf9c6f0daf00a059b6b50c86b4099b92bee8d1b
Elmin-web <elmin555@bk.ru>
2023-06-08 18:13:02 +0400

    fix

--- client/src/components/cards/Cards.jsx
+++ client/src/components/cards/Cards.jsx
@@ -1,21 +1,23 @@
-
 function Cards() {
-    return (
-        <div className="container">
-
-            <div className="container_card">
-                <div className="container_card-img">
-                    <img src="" alt="" />
-                </div>
-                <div className="container_card-text">
-                  <span><h1></h1></span>
-                  <span><h2></h2></span>  
-                </div>
-                <div className="container_card-description">
-                    <p></p>
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="container_card">
+        <div className="container_card-img">
+          <img src="" alt="" />
         </div>
-    )
+        <div className="container_card-text">
+          <span>
+            <h1></h1>
+          </span>
+          <span>
+            <h2></h2>
+          </span>
+        </div>
+        <div className="container_card-description">
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Cards;
