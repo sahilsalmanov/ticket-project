b5488fe23b1e2f4a82250f62c04fc93e19909535
Ilaha E. Shukurova <ilahaesh@code.local>
2023-06-08 17:25:00 +0400

    cards

---
+++ client/src/components/cards/Cards.jsx
@@ -0,0 +1,21 @@
+
+function Cards() {
+    return (
+        <div className="container">
+
+            <div className="container_card">
+                <div className="container_card-img">
+                    <img src="" alt="" />
+                </div>
+                <div className="container_card-text">
+                  <span><h1></h1></span>
+                  <span><h2></h2></span>  
+                </div>
+                <div className="container_card-description">
+                    <p></p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Cards;
